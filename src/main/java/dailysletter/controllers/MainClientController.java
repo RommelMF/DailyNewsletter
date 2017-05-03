1960b9867a0b674cc8636cbe7fb2eaa79b3f64cf
Rommel <dragondrum@mail.ru>
2017-05-03 08:31:55 +0300

    update controller

--- src/main/java/dailysletter/controllers/MainClientController.java
+++ src/main/java/dailysletter/controllers/MainClientController.java
@@ -1,8 +1,10 @@
 package dailysletter.controllers;
 
+import dailysletter.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Home on 02.05.2017.
@@ -14,4 +16,12 @@ public class MainClientController {
     public String getIndex() {
         return "index";
     }
+
+    @RequestMapping(name ="/dailynewsletter/authoriz")
+    public ModelAndView authoriz() {
+        return new ModelAndView("authoriz","user", new User());
+    }
+
+
+
 }
---
+++ src/main/java/dailysletter/models/User.java
@@ -0,0 +1,52 @@
+package dailysletter.models;
+
+/**
+ * Created by Home on 03.05.2017.
+ */
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private String login;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
